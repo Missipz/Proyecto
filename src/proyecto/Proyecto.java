54b14da560dda5575df18e8ca51abb467d5df4fe
Missi <Missi@DESKTOP-UCRCH80>
2020-08-08 20:44:12 -0600

    Pruebas de subida

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -19,6 +19,7 @@ public class Proyecto {
     public static void main(String[] args) {
         // TODO code application logic here
          JOptionPane.showMessageDialog(null,"Proyecto");
+         JOptionPane.showMessageDialog(null,"Proyecto1");
     }
     
 }
